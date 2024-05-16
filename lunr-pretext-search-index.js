49cbe38aa7a3145a64d4d4e89a294fe63de969de
ngeisen <161631248+ngeisen@users.noreply.github.com>
2024-05-16 16:41:47 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1,9 +1,9 @@
 var ptx_lunr_search_style = "textbook";
 var ptx_lunr_docs = [
 {
-  "id": "colophon-1",
+  "id": "frontmatter-2",
   "level": "1",
-  "url": "colophon-1.html",
+  "url": "frontmatter-2.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
@@ -19,72 +19,72 @@ var ptx_lunr_docs = [
   "body": " Types of Data    At the end of this section you will able to     Understand and apply the vocabulary associated with statistics.    Distinguish between quantitative data and categorical data.      Statistsics is the science of collecting, describing, and analyzing data.  The subjects\/objects that we obtain information about are called the cases or units in a dataset. These are represented as the rows  A variable is any characteristic that is recorded for each case. The variables generally correspond to the columns in a data table.  A categorical variable divides the cases into groups, placing each case into exactly one of two or more categories.  A quantitative variable measures or records a numerical quantity for each case.  Discuss structure of data, how collected.  discuss types of studies, create naming   Directions: Put the letters in order.    A    B    C    Sample exercise What is ? We're being a bit tricky here... , where is the concatenation operator.  "
 },
 {
-  "id": "objectives-1",
+  "id": "sec-section-name-2",
   "level": "2",
-  "url": "sec-section-name.html#objectives-1",
+  "url": "sec-section-name.html#sec-section-name-2",
   "type": "Objectives",
   "number": "1.1",
   "title": "",
   "body": "  At the end of this section you will able to     Understand and apply the vocabulary associated with statistics.    Distinguish between quantitative data and categorical data.    "
 },
 {
-  "id": "p-5",
+  "id": "sec-section-name-3",
   "level": "2",
-  "url": "sec-section-name.html#p-5",
+  "url": "sec-section-name.html#sec-section-name-3",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "Statistsics "
 },
 {
-  "id": "p-6",
+  "id": "sec-section-name-4",
   "level": "2",
-  "url": "sec-section-name.html#p-6",
+  "url": "sec-section-name.html#sec-section-name-4",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "cases "
 },
 {
-  "id": "p-7",
+  "id": "sec-section-name-5",
   "level": "2",
-  "url": "sec-section-name.html#p-7",
+  "url": "sec-section-name.html#sec-section-name-5",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "variable "
 },
 {
-  "id": "p-8",
+  "id": "sec-section-name-6",
   "level": "2",
-  "url": "sec-section-name.html#p-8",
+  "url": "sec-section-name.html#sec-section-name-6",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "categorical variable "
 },
 {
-  "id": "p-9",
+  "id": "sec-section-name-7",
   "level": "2",
-  "url": "sec-section-name.html#p-9",
+  "url": "sec-section-name.html#sec-section-name-7",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "quantitative variable "
 },
 {
-  "id": "exercise-1",
+  "id": "sec-section-name-10",
   "level": "2",
-  "url": "sec-section-name.html#exercise-1",
+  "url": "sec-section-name.html#sec-section-name-10",
   "type": "Checkpoint",
   "number": "1.1.1",
   "title": "",
   "body": " Directions: Put the letters in order.    A    B    C   "
 },
 {
-  "id": "exercise-2",
+  "id": "sec-section-name-11",
   "level": "2",
-  "url": "sec-section-name.html#exercise-2",
+  "url": "sec-section-name.html#sec-section-name-11",
   "type": "Checkpoint",
   "number": "1.1.2",
   "title": "Sample exercise.",
@@ -100,9 +100,9 @@ var ptx_lunr_docs = [
   "body": " Graphing Data    At the end of this section you will able to     Graph data appropriately (with technology).    Describe data in words and with quantitative measures.     "
 },
 {
-  "id": "objectives-2",
+  "id": "sec-two-2",
   "level": "2",
-  "url": "sec-two.html#objectives-2",
+  "url": "sec-two.html#sec-two-2",
   "type": "Objectives",
   "number": "1.2",
   "title": "",
@@ -115,7 +115,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.3",
   "title": "Using StatKey",
-  "body": " Using StatKey   Graphing One Categorical Variable   How to graph students eye color with StatKey.  Open a web browswer and copy   Download the csv file Lab 1 Data.csv posted in Canvas and save to your computer where it is easily accessible. We are going to analyze the students eye colors.  Under the Descriptive Statistics and Graph column on the left, choose the One Categorical Variable .   Image of StatKey menu   The second option under descriptive statitics menu is the one categorical variable.    Click on the Upload File button and locate the data file on your computer.  Identify which variable you will analyze (Eye Color) and click OK. The program constructs a bar graph and frequency table. A sample output image is below.   Sample image of StatKey output.   Sample image of bar graph and frequency table of the students eye color.     Interpreting Bar Graphs   From the bar graph we can see that most students have brown eyes and green eyes occur the least.  From the frequency table we see the sample consisted of 63 UNC students. There are 34 students with brown eyes which is 54% of the class.  Using the graph above, which of the following statements are correct? Selelct all that apply.  "
+  "body": " Using StatKey   Graphing One Categorical Variable   How to graph students eye color with StatKey.  Open a web browswer and copy   Download the csv file Lab 1 Data.csv posted in Canvas and save to your computer where it is easily accessible. We are going to analyze the students eye colors.  Under the Descriptive Statistics and Graph column on the left, choose the One Categorical Variable .   Image of StatKey menu   The second option under descriptive statitics menu is the one categorical variable.    Click on the Upload File button and locate the data file on your computer.  Identify which variable you will analyze (Eye Color) and click OK. The program constructs a bar graph and frequency table. A sample output image is below.   Sample image of StatKey output.   Sample image of bar graph and frequency table of the students eye color.     Interpreting Bar Graphs   From the bar graph we can see that most students have brown eyes and green eyes occur the least.  From the frequency table we see the sample consisted of 63 UNC students. There are 34 students with brown eyes which is 54% of the class.  Using the graph above, which of the following statements are correct? Selelct all that apply.  Interpreting information from a graph  Using the graph above, select all statements that apply.     Brown eyes occur most often for these students.    This is represented by the tallest bar.      Blue eyes occurred 22.2%    Take the frequency count and divide by the sample size for the percentage.      The sample size is 54 students.    Look at the total from the frequency table.      The distribution is right skewed.    We do not discuss distribution shape with categorical data.     "
 },
 {
   "id": "OneCatStatKey",
@@ -136,13 +136,22 @@ var ptx_lunr_docs = [
   "body": " Sample image of StatKey output.   Sample image of bar graph and frequency table of the students eye color.   "
 },
 {
-  "id": "p-29",
+  "id": "sec-three-11",
   "level": "2",
-  "url": "sec-three.html#p-29",
+  "url": "sec-three.html#sec-three-11",
   "type": "Paragraph (with a defined term)",
   "number": "",
   "title": "",
   "body": "Interpreting Bar Graphs "
+},
+{
+  "id": "some-matching",
+  "level": "2",
+  "url": "sec-three.html#some-matching",
+  "type": "Checkpoint",
+  "number": "1.3.3",
+  "title": "Interpreting information from a graph.",
+  "body": "Interpreting information from a graph  Using the graph above, select all statements that apply.     Brown eyes occur most often for these students.    This is represented by the tallest bar.      Blue eyes occurred 22.2%    Take the frequency count and divide by the sample size for the percentage.      The sample size is 54 students.    Look at the total from the frequency table.      The distribution is right skewed.    We do not discuss distribution shape with categorical data.    "
 },
 {
   "id": "ChapterTwo",
@@ -178,21 +187,102 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "4.2",
   "title": "Creating Bootstrap Distributions",
-  "body": " Creating Bootstrap Distributions  "
+  "body": " Creating Bootstrap Distributions  Distribution Matching Problem  Match the type of distribution with the appropriate description.    sampling distribution  Distribution of statistics computed for different samples of the same size taken from the population, centered at the parameter    bootstrap distribution  Distribution of bootstrap statistics taken with replacement from one original sample centered at the original sample statistic    randomization distribution  Distribution of random statistics we would observe just by random chance assuming Ho is true, center around Ho value    "
 },
 {
-  "id": "ChapterTesting",
-  "level": "1",
-  "url": "ChapterTesting.html",
-  "type": "Chapter",
-  "number": "5",
-  "title": "Hypothesis Testing",
-  "body": " Hypothesis Testing  "
+  "id": "matching",
+  "level": "2",
+  "url": "sec-bootdist.html#matching",
+  "type": "Checkpoint",
+  "number": "4.2.1",
+  "title": "Distribution Matching Problem.",
+  "body": "Distribution Matching Problem  Match the type of distribution with the appropriate description.    sampling distribution  Distribution of statistics computed for different samples of the same size taken from the population, centered at the parameter    bootstrap distribution  Distribution of bootstrap statistics taken with replacement from one original sample centered at the original sample statistic    randomization distribution  Distribution of random statistics we would observe just by random chance assuming Ho is true, center around Ho value   "
 },
 {
-  "id": "colophon-2",
+  "id": "sec-advanced-topics",
   "level": "1",
-  "url": "colophon-2.html",
+  "url": "sec-advanced-topics.html",
+  "type": "Section",
+  "number": "5.1",
+  "title": "Hypothesis Test",
+  "body": " Hypothesis Test   Which name belongs to the face on the left?      Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.    Have student submit answer - collect class data.  A printable version (worksheet) of this invetigation, plus questions, is available here: Facial Prototyping .  *Note that this file is stored in your assets folder, and is automatically put in external when pretext build the file.   Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  Use StatKey to answer the first two checkpoint questions below. Click here to access the actual article.     HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.   We're being a bit tricky here...   , where is the concatenation operator.    CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.   We're being a bit tricky here...   , where is the concatenation operator.     Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.     Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.     The video below would be a demo of one of us using StatKey.   StatKey for the Hypothesis Test  Hello World    "
+},
+{
+  "id": "sec-advanced-topics-2",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-2",
+  "type": "Investigation",
+  "number": "5.1.1",
+  "title": "Which name belongs to the face on the left?",
+  "body": " Which name belongs to the face on the left?    "
+},
+{
+  "id": "BOBorTIM",
+  "level": "2",
+  "url": "sec-advanced-topics.html#BOBorTIM",
+  "type": "Figure",
+  "number": "5.1.1",
+  "title": "",
+  "body": " Photograph in the assets directory.   Photograph of .....  Photographs of faces of two men.   "
+},
+{
+  "id": "sec-advanced-topics-7",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-7",
+  "type": "Example",
+  "number": "5.1.2",
+  "title": "Facial Prototyping.",
+  "body": " Facial Prototyping  A study in Psychonomic Bulletin and Review (Lea, Thomas, Lamkin, and Bell, 2007) presented evidence that people use facial prototypes when they encounter different names. Participants were given two faces and asked to identify which one was Tim and which one was Bob. The researchers wrote that their participants “overwhelmingly agreed” on which face belonged to Tim and which face belonged to Bob but did not provide the exact results of their study.  Use StatKey to answer the first two checkpoint questions below. Click here to access the actual article.  "
+},
+{
+  "id": "sec-advanced-topics-9",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-9",
+  "type": "Checkpoint",
+  "number": "5.1.3",
+  "title": "HT -Manually-authored exercise.",
+  "body": "HT -Manually-authored exercise  Perform a full hypothesis test to determine whether UNC students, in general, use facial prototypes when they encounter different names. Use a 5% significance level. Let the parameter of interest be the probability that a student would assign the name Tim to the face on the left.  "
+},
+{
+  "id": "sec-advanced-topics-13",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-13",
+  "type": "Checkpoint",
+  "number": "5.1.4",
+  "title": "CI -Manually-authored exercise.",
+  "body": "CI -Manually-authored exercise  Also compute and interpret a 95% confidence interval for the parameter of interest.  "
+},
+{
+  "id": "true-false",
+  "level": "2",
+  "url": "sec-advanced-topics.html#true-false",
+  "type": "Checkpoint",
+  "number": "5.1.5",
+  "title": "Decision CI - Runestone-powered True\/False Question.",
+  "body": "Decision CI - Runestone-powered True\/False Question  Based on the confidence interval, we would NOT reject the null hypothesis.   The null value is outside of the CI.  "
+},
+{
+  "id": "some-matching",
+  "level": "2",
+  "url": "sec-advanced-topics.html#some-matching",
+  "type": "Checkpoint",
+  "number": "5.1.6",
+  "title": "Practical Import - Runestone-powered Matching Problem.",
+  "body": "Practical Import - Runestone-powered Matching Problem  Does your confidence interval suggest that the results are practically important? Explain.     Yes, because....    answer specific feedback      Yes, because......    answer specific feedback      No, because...    answer specific feedback      Question does not apply.    Does not apply if results were not statistically significant.    "
+},
+{
+  "id": "sec-advanced-topics-21",
+  "level": "2",
+  "url": "sec-advanced-topics.html#sec-advanced-topics-21",
+  "type": "Figure",
+  "number": "5.1.7",
+  "title": "StatKey for the Hypothesis Test",
+  "body": " StatKey for the Hypothesis Test  Hello World   "
+},
+{
+  "id": "backmatter-2",
+  "level": "1",
+  "url": "backmatter-2.html",
   "type": "Colophon",
   "number": "",
   "title": "Colophon",
